1cdec083f7f6b5e4ff8254461c2ac5de925d5682
RuslanDoel <RuslanDoel@users.noreply.github.com>
2014-05-19 10:48:47 +0700

    index
    php

---
+++ index.php
@@ -0,0 +1,47 @@
+PROJECT-TUBES1
+==============
+
+
+==============
+
+JURUSAN TEKNIK INFORMATIKA 
+
+
+FAKULTAS TEKNIK INFORMATIKA
+ 
+
+IVERSITAS SILIWANGI
+
+============================
+
+
+TUGAS LAYANAN WEB [GITHUB](https://github.com)
+
+
+GitHub adalah layanan berbasis web hosting untuk proyek-proyek pengembangan perangkat lunak yang menggunakan sistem kontrol revisi Git. GitHub menawarkan rencana dibayar untuk repositori pribadi, dan akun gratis untuk proyek-proyek open source. Pada Mei 2011, GitHub adalah sumber yang paling populer kode situs repositori terbuka.
+
+GitHub juga merupakan tempat terbaik untuk berbagi kode dengan teman-teman, rekan kerja, teman sekelas, dan orang asing. Lebih dari dua juta orang menggunakan GitHub untuk membangun hal-hal menakjubkan bersama-sama.
+
+Dengan fitur kolaboratif GitHub.com, desktop dan aplikasi mobile, dan GitHub Enterprise, belum pernah lebih mudah bagi individu dan tim untuk menulis kode yang lebih baik, lebih cepat.
+
+
+[GOAL](http://i.goal.com/id-ID/)
+
+
+
+
+![117006161](https://github.com/RuslanDoel) 
+
+
+![117006161](https://github.com/rizkiabdillahrusyawan)
+
+
+![Project-Tubes1-RIZKI 117006156--RUSLAN 117006161--RYAN 117006189]
+<?php
+ $feed = file_get_contents("http://www.kompas.com/getrss/bisniskeuangan");
+ 
+ // http://cariprogram.blogspot.com
+ // http://i.goal.com/id-ID/
+ 
+ //$feed = file_get_contents($_GET['url']);
+ $xml = new SimpleXmlElement($feed, LIBXML_NOCDATA);
